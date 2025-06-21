26243d915ad6cb037df5a0ddccb2732ea06f9c8f
Artem Zhmura <99594471+Somnovem@users.noreply.github.com>
2025-06-21 21:23:17 +0300

    removing unnecessary sql scripts

--- LiveVibe.Server/Scripts/AvailableSeatsConstraint.sql
+++
@@ -1,8 +0,0 @@
-IF NOT EXISTS (
-    SELECT * FROM sys.check_constraints 
-    WHERE name = 'CHK_AvailableSeats_NonNegative'
-)
-BEGIN
-    ALTER TABLE Event_Seat_Types
-    ADD CONSTRAINT CHK_AvailableSeats_NonNegative CHECK (AvailableSeats >= 0);
-END;
